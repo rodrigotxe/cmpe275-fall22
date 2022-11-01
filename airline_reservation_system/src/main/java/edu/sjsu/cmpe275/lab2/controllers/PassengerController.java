e8f2ca412d58be73cb9244eeec4bf2492ef705e5
Rodrigo Colasso <rodrigocolasso@gmail.com>
2022-11-01 16:07:22 -0700

    Create/update passenger

--- airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/controllers/PassengerController.java
+++ airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/controllers/PassengerController.java
@@ -1,5 +1,8 @@
 package edu.sjsu.cmpe275.lab2.controllers;
 
+import edu.sjsu.cmpe275.lab2.services.PassengerService;
+import edu.sjsu.cmpe275.lab2.services.PassengerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,9 @@ import edu.sjsu.cmpe275.lab2.entities.Passenger;
 @CrossOrigin(origins = "*")
 public class PassengerController {
 
+	@Autowired
+	private PassengerServiceImpl passengerService;
+
 	@RequestMapping(value = "/passenger/{id}")
 	public Passenger getPassenger(@PathVariable("id") String id, @RequestParam("xml") String xml) {
 		// TODO
@@ -21,19 +27,39 @@ public class PassengerController {
 
 	@RequestMapping(value = "/passenger", method = RequestMethod.POST)
 	public Passenger createPassenger(@RequestParam("firstname") String firstName,
-			@RequestParam("lastname") String lastName, @RequestParam("birthyear") String birthYear,
+			@RequestParam("lastname") String lastName, @RequestParam("birthyear") int birthYear,
 			@RequestParam("gender") String gender, @RequestParam("phone") String phone,
 			@RequestParam("xml") String xml) {
-		// TODO
+
+		Passenger newPassenger = new Passenger();
+
+		newPassenger.setFirstName( firstName );
+		newPassenger.setLastName( lastName );
+		newPassenger.setBirthYear( birthYear );
+		newPassenger.setGender( gender );
+		newPassenger.setPhone( phone );
+
+		passengerService.addPassenger( newPassenger );
+
 		return null;
 	}
 
 	@RequestMapping(value = "/passenger/{id}", method = RequestMethod.PUT)
 	public Passenger updatePassenger(@PathVariable("id") String id, @RequestParam("firstname") String firstName,
-			@RequestParam("lastname") String lastName, @RequestParam("birthyear") String birthYear,
+			@RequestParam("lastname") String lastName, @RequestParam("birthyear") int birthYear,
 			@RequestParam("gender") String gender, @RequestParam("phone") String phone,
 			@RequestParam("xml") String xml) {
-		// TODO
+
+		Passenger passenger = passengerService.getPassenger( id );
+
+		passenger.setFirstName( firstName );
+		passenger.setLastName( lastName );
+		passenger.setBirthYear( birthYear );
+		passenger.setGender( gender );
+		passenger.setPhone( phone );
+
+		passengerService.updatePassenger( passenger );
+
 		return null;
 	}
 
--- airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/services/PassengerServiceImpl.java
+++ airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/services/PassengerServiceImpl.java
@@ -1,8 +1,48 @@
 package edu.sjsu.cmpe275.lab2.services;
 
+import edu.sjsu.cmpe275.lab2.entities.Passenger;
+import edu.sjsu.cmpe275.lab2.repos.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PassengerServiceImpl implements PassengerService {
+    
+    private final PassengerRepository passengerRepository;
+
+    @Autowired
+    public PassengerServiceImpl( PassengerRepository passengerRepository ) {
+
+        this.passengerRepository = passengerRepository;
+
+    }
+
+    public Passenger getPassenger( String id ) {
+
+        return new Passenger();
+
+    }
+
+    public void addPassenger( Passenger newPassenger ) {
+
+        passengerRepository.save( newPassenger );
+
+    }
+
+    public void updatePassenger( Passenger passenger ) {
+
+        checkPassengerID( passenger.getId() );
+
+        passengerRepository.save( passenger );
+
+    }
+
+    private void checkPassengerID( String passengerID ) {
+
+        if( ! passengerRepository.existsById( passengerID ) )
+
+            throw new IllegalStateException("passengerID: Passenger ID " + passengerID + " does not exits");
+
+    }
 
 }
