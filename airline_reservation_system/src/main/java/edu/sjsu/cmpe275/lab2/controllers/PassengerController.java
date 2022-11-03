6951464cbaa6ed3b62ca7a2ef00a9e82762aadd9
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-11-02 23:40:28 -0700

    Code Refactor

--- airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/controllers/PassengerController.java
+++ airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/controllers/PassengerController.java
@@ -24,7 +24,7 @@ public class PassengerController {
 
 	@Autowired
 	private PassengerService passengerService;
-	
+
 	private static final String BAD_REQUEST = "BadRequest";
 	private static final String SUCCESS = "Success";
 
@@ -32,24 +32,27 @@ public class PassengerController {
 			MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<?> getPassenger(@PathVariable("id") String id, @RequestParam("xml") String xml) {
 		Passenger passenger = passengerService.getPassenger(id);
+		boolean xmlView = "true".equals(xml);
 
 		HttpHeaders headers = new HttpHeaders();
 
-		if ("true".equals(xml))
+		if (xmlView)
 			headers.setContentType(MediaType.APPLICATION_XML);
 
 		if (passenger == null) {
-			Response response = new Response();
-			response.setCode("404");
-			response.setMsg("Sorry, the requested passenger with ID " + id + " does not exist");
-
-			if ("true".equals(xml)) {
-				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
-			}
-
-			HashMap<String, Response> map = new HashMap<>();
-			map.put(BAD_REQUEST, response);
-			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+//			Response response = new Response();
+//			response.setCode("404");
+//			response.setMsg("Sorry, the requested passenger with ID " + id + " does not exist");
+//
+//			if ("true".equals(xml)) {
+//				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
+//			}
+//
+//			HashMap<String, Response> map = new HashMap<>();
+//			map.put(BAD_REQUEST, response);
+//			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+			return customResponse("404", "Sorry, the requested passenger with ID " + id + " does not exist",
+					BAD_REQUEST, xmlView, headers, HttpStatus.NOT_FOUND);
 		}
 
 		return new ResponseEntity<Passenger>(passenger, headers, HttpStatus.OK);
@@ -63,26 +66,29 @@ public class PassengerController {
 			@RequestParam("xml") String xml) {
 
 		Passenger passenger = passengerService.findByPhone(phone);
-		
+		boolean xmlView = "true".equals(xml);
+
 		HttpHeaders headers = new HttpHeaders();
 
-		if ("true".equals(xml))
+		if (xmlView)
 			headers.setContentType(MediaType.APPLICATION_XML);
-		
+
 		if (passenger != null) {
-			Response response = new Response();
-			response.setCode("400");
-			response.setMsg("Another passenger with the same number " + phone + " already exists");
-			
-			if ("true".equals(xml)) {
-				return new ResponseEntity<Response>(response, headers, HttpStatus.BAD_REQUEST);
-			}
-			
-			HashMap<String, Response> map = new HashMap<>();
-			map.put(BAD_REQUEST, response);
-			return new ResponseEntity<>(map, headers, HttpStatus.BAD_REQUEST);
+//			Response response = new Response();
+//			response.setCode("400");
+//			response.setMsg("Another passenger with the same number " + phone + " already exists");
+//
+//			if ("true".equals(xml)) {
+//				return new ResponseEntity<Response>(response, headers, HttpStatus.BAD_REQUEST);
+//			}
+//
+//			HashMap<String, Response> map = new HashMap<>();
+//			map.put(BAD_REQUEST, response);
+//			return new ResponseEntity<>(map, headers, HttpStatus.BAD_REQUEST);
+			return customResponse("400", "Another passenger with the same number " + phone + " already exists",
+					BAD_REQUEST, xmlView, headers, HttpStatus.BAD_REQUEST);
 		}
-		
+
 		Passenger newPassenger = new Passenger();
 
 		newPassenger.setFirstName(firstName);
@@ -92,7 +98,7 @@ public class PassengerController {
 		newPassenger.setPhone(phone);
 
 		Passenger createdPassenger = passengerService.addPassenger(newPassenger);
-		
+
 		return new ResponseEntity<Passenger>(createdPassenger, headers, HttpStatus.OK);
 	}
 
@@ -104,24 +110,27 @@ public class PassengerController {
 			@RequestParam("xml") String xml) {
 
 		Passenger passenger = passengerService.getPassenger(id);
-		
+		boolean xmlView = "true".equals(xml);
+
 		HttpHeaders headers = new HttpHeaders();
 
-		if ("true".equals(xml))
+		if (xmlView)
 			headers.setContentType(MediaType.APPLICATION_XML);
 
 		if (passenger == null) {
-			Response response = new Response();
-			response.setCode("404");
-			response.setMsg("Sorry, the requested passenger with ID " + id + " does not exist");
-
-			if ("true".equals(xml)) {
-				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
-			}
-
-			HashMap<String, Response> map = new HashMap<>();
-			map.put(BAD_REQUEST, response);
-			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+//			Response response = new Response();
+//			response.setCode("404");
+//			response.setMsg("Sorry, the requested passenger with ID " + id + " does not exist");
+//
+//			if ("true".equals(xml)) {
+//				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
+//			}
+//
+//			HashMap<String, Response> map = new HashMap<>();
+//			map.put(BAD_REQUEST, response);
+//			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+			return customResponse("404", "Sorry, the requested passenger with ID " + id + " does not exist",
+					BAD_REQUEST, xmlView, headers, HttpStatus.NOT_FOUND);
 		}
 
 		passenger.setFirstName(firstName);
@@ -139,38 +148,58 @@ public class PassengerController {
 			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<?> deletePassenger(@PathVariable("id") String id, @RequestParam("xml") String xml) {
 		Passenger passenger = passengerService.getPassenger(id);
-		
+		boolean xmlView = "true".equals(xml);
+
 		HttpHeaders headers = new HttpHeaders();
 
-		if ("true".equals(xml))
+		if (xmlView)
 			headers.setContentType(MediaType.APPLICATION_XML);
 
 		if (passenger == null) {
-			Response response = new Response();
-			response.setCode("404");
-			response.setMsg("Passenger with ID " + id + " does not exist");
-
-			if ("true".equals(xml)) {
-				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
-			}
-
-			HashMap<String, Response> map = new HashMap<>();
-			map.put(BAD_REQUEST, response);
-			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+//			Response response = new Response();
+//			response.setCode("404");
+//			response.setMsg("Passenger with ID " + id + " does not exist");
+//
+//			if ("true".equals(xml)) {
+//				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
+//			}
+//
+//			HashMap<String, Response> map = new HashMap<>();
+//			map.put(BAD_REQUEST, response);
+//			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+			return customResponse("404", "Passenger with ID " + id + " does not exist", BAD_REQUEST, xmlView, headers,
+					HttpStatus.NOT_FOUND);
 		}
-		
-		passengerService.deletePassenger(id);
-		
-		Response response = new Response();
-		response.setCode("200");
-		response.setMsg("Passenger with ID " + id + " deleted successfully");
 
-		if ("true".equals(xml)) {
-			return new ResponseEntity<Response>(response, headers, HttpStatus.OK);
+		passengerService.deletePassenger(id);
+
+//		Response response = new Response();
+//		response.setCode("200");
+//		response.setMsg("Passenger with ID " + id + " deleted successfully");
+//
+//		if ("true".equals(xml)) {
+//			return new ResponseEntity<Response>(response, headers, HttpStatus.OK);
+//		}
+//
+//		HashMap<String, Response> map = new HashMap<>();
+//		map.put(SUCCESS, response);
+//		return new ResponseEntity<>(map, headers, HttpStatus.OK);
+		return customResponse("200", "Passenger with ID " + id + " deleted successfully", SUCCESS, xmlView, headers,
+				HttpStatus.OK);
+	}
+
+	private ResponseEntity<?> customResponse(String code, String message, String tag, boolean xmlView,
+			HttpHeaders headers, HttpStatus status) {
+		Response response = new Response();
+		response.setCode(code);
+		response.setMsg(message);
+
+		if (xmlView) {
+			return new ResponseEntity<Response>(response, headers, status);
 		}
 
 		HashMap<String, Response> map = new HashMap<>();
-		map.put(SUCCESS, response);
-		return new ResponseEntity<>(map, headers, HttpStatus.OK);
+		map.put(tag, response);
+		return new ResponseEntity<>(map, headers, status);
 	}
 }
