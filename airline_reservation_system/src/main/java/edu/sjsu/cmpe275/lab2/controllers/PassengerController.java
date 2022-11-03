af6fc5617d6f3317bfbfa9a49291ec456bf9978a
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-11-02 20:53:27 -0700

    Added support for Error Response

--- airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/controllers/PassengerController.java
+++ airline_reservation_system/src/main/java/edu/sjsu/cmpe275/lab2/controllers/PassengerController.java
@@ -1,6 +1,10 @@
 package edu.sjsu.cmpe275.lab2.controllers;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.sjsu.cmpe275.lab2.entities.Passenger;
+import edu.sjsu.cmpe275.lab2.response.Response;
 import edu.sjsu.cmpe275.lab2.services.PassengerService;
 
 @RestController
@@ -20,13 +25,35 @@ public class PassengerController {
 	@Autowired
 	private PassengerService passengerService;
 
-	@RequestMapping(value = "/passenger/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/passenger/{id}", produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<?> getPassenger(@PathVariable("id") String id, @RequestParam("xml") String xml) {
 		Passenger passenger = passengerService.getPassenger(id);
-		return ResponseEntity.ok().contentType("true".equals(xml) ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON).body(passenger);
+
+		HttpHeaders headers = new HttpHeaders();
+
+		if ("true".equals(xml))
+			headers.setContentType(MediaType.APPLICATION_XML);
+
+		if (passenger == null) {
+			Response response = new Response();
+			response.setCode("404");
+			response.setMsg("Sorry, the requested passenger with ID " + id + " does not exist");
+
+			if ("true".equals(xml)) {
+				return new ResponseEntity<Response>(response, headers, HttpStatus.NOT_FOUND);
+			}
+
+			HashMap<String, Object> map = new HashMap<>();
+			map.put("BadRequest", response);
+			return new ResponseEntity<>(map, headers, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Passenger>(passenger, headers, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/passenger", method = RequestMethod.POST, produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/passenger", method = RequestMethod.POST, produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<?> createPassenger(@RequestParam("firstname") String firstName,
 			@RequestParam("lastname") String lastName, @RequestParam("birthyear") int birthYear,
 			@RequestParam("gender") String gender, @RequestParam("phone") String phone,
@@ -34,18 +61,21 @@ public class PassengerController {
 
 		Passenger newPassenger = new Passenger();
 
-		newPassenger.setFirstName( firstName );
-		newPassenger.setLastName( lastName );
-		newPassenger.setBirthYear( birthYear );
-		newPassenger.setGender( gender );
-		newPassenger.setPhone( phone );
+		newPassenger.setFirstName(firstName);
+		newPassenger.setLastName(lastName);
+		newPassenger.setBirthYear(birthYear);
+		newPassenger.setGender(gender);
+		newPassenger.setPhone(phone);
 
-		Passenger createdPassenger = passengerService.addPassenger( newPassenger );
+		Passenger createdPassenger = passengerService.addPassenger(newPassenger);
 
-		return ResponseEntity.ok().contentType("true".equals(xml) ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON).body(createdPassenger);
+		return ResponseEntity.ok()
+				.contentType("true".equals(xml) ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON)
+				.body(createdPassenger);
 	}
 
-	@RequestMapping(value = "/passenger/{id}", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/passenger/{id}", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<?> updatePassenger(@PathVariable("id") String id, @RequestParam("firstname") String firstName,
 			@RequestParam("lastname") String lastName, @RequestParam("birthyear") int birthYear,
 			@RequestParam("gender") String gender, @RequestParam("phone") String phone,
@@ -53,18 +83,21 @@ public class PassengerController {
 
 		Passenger passenger = new Passenger();
 
-		passenger.setFirstName( firstName );
-		passenger.setLastName( lastName );
-		passenger.setBirthYear( birthYear );
-		passenger.setGender( gender );
-		passenger.setPhone( phone );
+		passenger.setFirstName(firstName);
+		passenger.setLastName(lastName);
+		passenger.setBirthYear(birthYear);
+		passenger.setGender(gender);
+		passenger.setPhone(phone);
 
-		Passenger updatedPassenger = passengerService.updatePassenger( passenger );
+		Passenger updatedPassenger = passengerService.updatePassenger(passenger);
 
-		return ResponseEntity.ok().contentType("true".equals(xml) ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON).body(updatedPassenger);
+		return ResponseEntity.ok()
+				.contentType("true".equals(xml) ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON)
+				.body(updatedPassenger);
 	}
 
-	@RequestMapping(value = "/passenger/{id}", method = RequestMethod.DELETE, produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/passenger/{id}", method = RequestMethod.DELETE, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<?> deletePassenger(@PathVariable("id") String id, @RequestParam("xml") String xml) {
 		passengerService.deletePassenger(id);
 		return null;
